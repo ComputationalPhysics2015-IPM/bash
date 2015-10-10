52ec9cc67c3d1262c9e0385078443513653c7013
mrshnra <mrshnra@gmail.com>
2015-10-10 12:22:16 +0330

    Update exc02.sh

--- exc02.sh
+++ exc02.sh
@@ -2,4 +2,8 @@
 # Write an script that accepts a file name and a number as its parameter, 
 # Then return the content of that line number in that file. 
 
-
+echo -n "Enter the file name"
+read name
+echo -n "Enter a number"
+read number
+sed -n ''$number' p' $name
